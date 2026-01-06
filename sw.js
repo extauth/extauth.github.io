05f5ac9b11b2c590a5b5550aa80a695c1dc829bb
Alpx <alpxit@gmail.com>
2026-01-06 17:20:25 +0300

    related with PWA code

--- sw.js
+++ sw.js
@@ -1,17 +1,29 @@
+const CACHE_NAME = 'qrauth-v1.1.5';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './favicon.svg',
+  "./frw/bootstrap.min.css",
+  "./frw/jquery.min.js",
+  "./frw/bootstrap.bundle.min.js",
+  "./frw/age.min.js",
+  "./frw/qrcode.min.js",
+  "./frw/qr-scanner.legacy.min.js",
+  "./frw/totp.js",
+  "./qrauth.js"
+];
+
 self.addEventListener('install', (event) => {
-  /*event.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(resourcesToCache);
-    })
-  );*/
+  event.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-  // Intercept requests and serve from cache if offline
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
+      caches.match(event.request).then((cachedResponse) => {
+        return cachedResponse || fetch(event.request);
       })
   );
 });
